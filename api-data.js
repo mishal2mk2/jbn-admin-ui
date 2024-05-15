6fa9dbb93af9b2b8ba313123d9243c8d0b3b2a22
Mohammed Mishal <mishal2mk2@gmail.com>
2024-05-16 01:10:56 +0530

    updated api data

--- api-data.js
+++ api-data.js
@@ -14,23 +14,99 @@ const CreateNewProject = {
   orderStatus: 1,
 };
 
-
 const addDrawingFile = {
   file: File,
   notes: "",
 };
 
-const addMaterialFile = {
+const estimateMaterial = {
   item: [
     { item_id: "123", quantity: 2, subTotal: 222 },
     { item_id: "124", quantity: 2, subTotal: 222 },
     //more items
   ],
-  grossTotal:1234,
-  isApproved:false,
+  //estimate price of order
+  grossTotal: 1234,
+  isApproved: false,
+  //estimate time of completion
+  estimatedDaysOfCompletion: 12,
+  notes: "",
 };
 
-const estimateMaterial = {
-    estimatedDaysOfCompletion:12,
+const awaitingConfirmation = {
+  transactionId: "",
+  dateOfTransaction: Date,
+};
+
+const estimateMaterialArrival = {
+  estimateDateOfArrival: Date,
+  priority: "High" || "Low" || "Critical" || "Medium",
+};
+
+const Production = {
+  productionStatus: [
+    {
+        status:1,
+        isStarted: true,
+        completed: 10, //in percentage should be less than 100 and not less than 0
+    },
+    {
+        status:2,
+        isStarted: true,
+        completed: 5, //in percentage should be less than 100 and not less than 0
+    },
+    {
+        status:3,
+        isStarted: false,
+        completed: 0, //in percentage should be less than 100 and not less than 0
+    },
+    {
+        status:4,
+        isStarted: false,
+        completed: 0, //in percentage should be less than 100 and not less than 0
+    },
+    {
+        status:5,
+        isStarted: false,
+        completed: 0, //in percentage should be less than 100 and not less than 0
+    },
+    {
+        status:6,
+        isStarted: false,
+        completed: 0, //in percentage should be less than 100 and not less than 0
+    },
+  ],
+  currentStatus: 2,//latest started status
+  isProductionComplete:false
+};
+
+const delivery={
+    driverNumber:987654321,
+    deliveryVehicleNumber:'KL11AD2234',
+}
+
+const installation = {
+    uploadedImages:File,
     notes:''
+}
+
+const awaitingService = {
+    //no form it is just status
+}
+
+const service = {
+    serviceReport:File,
+    notes:'',
+    closingReport:File,
+}
+
+const Closed ={
+    //project successfully completed and closed 
+    //button to get closing report
+}
+
+const Cancelled ={
+    //project cancelled 
+    //button to restart project
+    //reason for cancellation
 }
