12f7a5d676c1992761837819a6ec3d7033dbefd1
Salmanul Fariz N <salmanulfariz997@gmail.com>
2024-05-19 02:43:27 +0530

    Complete the data setup of form 7 section

--- src/app/dashboard/partials/status-forms/form-7/form-7.component.ts
+++ src/app/dashboard/partials/status-forms/form-7/form-7.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-7',
   templateUrl: './form-7.component.html',
-  styleUrl: './form-7.component.css'
+  styleUrl: './form-7.component.css',
 })
-export class Form7Component {
+export class Form7Component implements OnInit {
+  FormGroupData!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.FormGroupData = this.formBuilder.group({
+      deliveryVehicleNumber: ['', Validators.required, Validators.minLength(4)],
+      driverNumber: ['', Validators.required, Validators.minLength(4)],
+      notes: [''],
+    });
+  }
+
+  formSubmit() {
+    // Check the form validation is complete
+    if (this.FormGroupData.invalid) {
+      this.FormGroupData.markAllAsTouched();
+      return;
+    }
+  }
 }
