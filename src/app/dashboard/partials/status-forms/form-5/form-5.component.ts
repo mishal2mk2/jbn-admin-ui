4e924bb635b5f2aac5225a4560535adfa0d98e43
Salmanul Fariz N <salmanulfariz997@gmail.com>
2024-05-18 23:38:11 +0530

    Complete the data setup of form 5 section

--- src/app/dashboard/partials/status-forms/form-5/form-5.component.ts
+++ src/app/dashboard/partials/status-forms/form-5/form-5.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-5',
   templateUrl: './form-5.component.html',
-  styleUrl: './form-5.component.css'
+  styleUrl: './form-5.component.css',
 })
-export class Form5Component {
+export class Form5Component implements OnInit {
+  FormGroupData!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.FormGroupData = this.formBuilder.group({
+      priority: ['', Validators.required],
+      estimateDateOfArrival: ['', Validators.required],
+      notes: [''],
+    });
+  }
+
+  formSubmit() {
+    // Check the form validation is complete
+    if (this.FormGroupData.invalid) {
+      this.FormGroupData.markAllAsTouched();
+      return;
+    }
+  }
 }
