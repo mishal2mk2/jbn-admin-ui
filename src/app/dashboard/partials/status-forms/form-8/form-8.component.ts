7f2813602fecaa846873199f777148454e6c03d1
Salmanul Fariz N <salmanulfariz997@gmail.com>
2024-05-19 02:51:06 +0530

    Complete the data setup of form 8 section

--- src/app/dashboard/partials/status-forms/form-8/form-8.component.ts
+++ src/app/dashboard/partials/status-forms/form-8/form-8.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormValidationService } from '../../common/service/form-validation.service';
 
 @Component({
   selector: 'app-form-8',
   templateUrl: './form-8.component.html',
-  styleUrl: './form-8.component.css'
+  styleUrl: './form-8.component.css',
 })
-export class Form8Component {
+export class Form8Component implements OnInit {
+  FormGroupData!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private _FormValidationService: FormValidationService
+  ) {}
+
+  ngOnInit(): void {
+    this.FormGroupData = this.formBuilder.group({
+      file: ['', Validators.required],
+      notes: [''],
+    });
+  }
+
+  // Validate File size on Add file
+  validateFileSize(event: Event) {
+    // Check the File size more the 5mb and if true
+    const { files } = event.target as HTMLInputElement;
+
+    if (files) {
+      this.FormGroupData.controls['file'].setErrors({
+        invalidFile: false,
+        maxSize: false,
+      });
+
+      // Check the is File
+      if (!this._FormValidationService.isValidImagePdfFileType(files)) {
+        this.FormGroupData.patchValue({
+          file: '',
+        });
+
+        this.FormGroupData.controls['file'].setErrors({
+          invalidFile: true,
+        });
+
+        return;
+      }
+
+      // Check the File size
+      const maxSize = 1 * 1024 * 1024; // 5MB in bytes
+      const selectFile = files[0];
+
+      if (selectFile?.size > maxSize) {
+        this.FormGroupData.patchValue({
+          file: '',
+        });
+
+        this.FormGroupData.controls['file'].setErrors({
+          maxSize: true,
+        });
+      }
+    }
+  }
+
+  formSubmit() {
+    // Check the form validation is complete
+    if (this.FormGroupData.invalid) {
+      this.FormGroupData.markAllAsTouched();
+      return;
+    }
+  }
 }
