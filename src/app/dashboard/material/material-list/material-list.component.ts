734de48307e2e26ae05c01a45582651b8b561ebf
Salmanul Fariz N <salmanulfariz997@gmail.com>
2024-05-20 05:01:12 +0530

    Added the material create section

--- src/app/dashboard/material/material-list/material-list.component.ts
+++ src/app/dashboard/material/material-list/material-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-material-list',
@@ -6,6 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './material-list.component.css',
 })
 export class MaterialListComponent {
+  @ViewChild('MaterialModal') defaultModal!: ElementRef;
+
   // Pagination variables
   currentPage = 1;
   itemsPerPage = 20;
@@ -38,7 +42,43 @@ export class MaterialListComponent {
     },
   ];
 
-  ngOnInit(): void {}
+  FormGroupData!: FormGroup;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.closeModal();
+
+    this.FormGroupData = this.formBuilder.group({
+      materialName: ['', Validators.required],
+      materialUnit: ['', Validators.required],
+      materialPhone: ['', Validators.required],
+    });
+  }
+
+  // Close the modal section
+  closeModal() {
+    const modal = this.defaultModal?.nativeElement as HTMLElement;
+    const modalOverlay = document.getElementById('modal-backdrop');
+
+    // Navigate to the same route with the query parameter
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: { id: null },
+      queryParamsHandling: 'merge',
+    });
+
+    if (modal && modalOverlay) {
+      //modal toggle settings
+      modal.classList.toggle('hidden');
+      modal.classList.toggle('flex');
+      modalOverlay.classList.toggle('hidden');
+    }
+  }
 
   // Function to get the current page of materials
   getCurrentPageMaterials() {
@@ -55,5 +95,25 @@ export class MaterialListComponent {
     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
     const endIndex = startIndex + this.itemsPerPage;
     return filteredMaterials.slice(startIndex, endIndex);
+  }
+
+  createMaterialModal() {
+    const modal = this.defaultModal.nativeElement as HTMLElement;
+    const modalOverlay = document.getElementById('modal-backdrop');
+
+    if (modal && modalOverlay) {
+      //modal toggle settings
+      modal.classList.toggle('hidden');
+      modal.classList.toggle('flex');
+      modalOverlay.classList.toggle('hidden');
+    }
+  }
+
+  // Check the Create Material form validation is complete
+  createMaterialformSubmit() {
+    if (this.FormGroupData.invalid) {
+      this.FormGroupData.markAllAsTouched();
+      return;
+    }
   }
 }
