d9071aaca6e76cdb4ec45327d08f4e6b03f0c278
Salmanul Fariz N <salmanulfariz997@gmail.com>
2024-05-20 03:06:16 +0530

    Added the user list section

--- src/app/dashboard/user/user-list/user-list.component.ts
+++ src/app/dashboard/user/user-list/user-list.component.ts
@@ -1,10 +1,89 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.css'
+  styleUrl: './user-list.component.css',
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  // Pagination variables
+  currentPage = 1;
+  itemsPerPage = 20;
+  searchTerm = ''; // Variable to store the search term
 
+  users = [
+    {
+      name: 'Salmanul Fariz',
+      username: 'salman',
+      phone: '84928492923',
+      role: 'HR',
+    },
+    {
+      name: 'Salmanul Fariz',
+      username: 'salman',
+      phone: '84928492923',
+      role: 'MD',
+    },
+    {
+      name: 'Salmanul Fariz',
+      username: 'salman',
+      phone: '84928492923',
+      role: 'GM',
+    },
+    {
+      name: 'Salmanul Fariz',
+      username: 'salman',
+      phone: '84928492923',
+      role: 'OM',
+    },
+    {
+      name: 'Salmanul Fariz',
+      username: 'salman',
+      phone: '84928492923',
+      role: 'PM',
+    },
+    {
+      name: 'Salmanul Fariz',
+      username: 'salman',
+      phone: '84928492923',
+      role: 'SV',
+    },
+    {
+      name: 'Salmanul Fariz',
+      username: 'salman',
+      phone: '84928492923',
+      role: 'WR',
+    },
+    {
+      name: 'Salmanul Fariz',
+      username: 'salman',
+      phone: '84928492923',
+      role: 'CU',
+    },
+    {
+      name: 'Salmanul Fariz',
+      username: 'salman',
+      phone: '84928492923',
+      role: 'US',
+    },
+  ];
+
+  ngOnInit(): void {}
+
+  // Function to get the current page of users
+  getCurrentPageUsers() {
+    const filteredUsers = this.users.filter(
+      (user) =>
+        user.username.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        user.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        user.phone
+          .toString()
+          .toLowerCase()
+          .includes(this.searchTerm.toLowerCase())
+    );
+
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return filteredUsers.slice(startIndex, endIndex);
+  }
 }
