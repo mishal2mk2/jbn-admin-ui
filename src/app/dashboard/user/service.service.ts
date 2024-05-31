321f68de8e5efd12822317ad59ffb486b137add4
mishalBasheer <mishal2mk2@gmail.com>
2024-05-31 23:46:12 +0530

    ui updates

--- src/app/dashboard/user/service.service.ts
+++ src/app/dashboard/user/service.service.ts
@@ -2,9 +2,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IUser } from './user.interface';
 import { getNsPrefix } from '@angular/compiler';
+import { environment } from '../../../environments/environment';
 
 
-const PREFIX_PATH = 'http://localhost:3000/user';
+const PREFIX_PATH = environment.API_ENDPOINT;
 const httpOptions = {
   headers : new HttpHeaders({'Content-Type':'application/json'}),
 }
@@ -17,19 +18,19 @@ export class ServiceService {
   constructor(private http:HttpClient) { }
 
   getUserLIst(){
-    const url = PREFIX_PATH +'/list';
+    const url = PREFIX_PATH +'/user/list';
     return this.http.post<{data:IUser[]}>(url,{});
   }
   updateUser(id:string,role:string){
-    const url = PREFIX_PATH +`/${id}`
+    const url = PREFIX_PATH +`/user/${id}`
     return this.http.put(url,{role});
   }
   blockUser(id:string){
-    const url = PREFIX_PATH +`/block/${id}`;
+    const url = PREFIX_PATH +`/user/block/${id}`;
     return this.http.get(url);
   }
   unBlockUser(id:string){
-      const url = PREFIX_PATH +`/unblock/${id}`;
+      const url = PREFIX_PATH +`/user/unblock/${id}`;
     return this.http.get(url);
   }
 }
